bfb99609c619b8a32b2ed765f369a3d84d8472b5
dks8064 <dks8064@naver.com>
2022-01-07 18:44:50 +0900

    modify Bfs/백조의 호수.cpp

--- Bfs/백조의 호수.cpp
+++ Bfs/백조의 호수.cpp
@@ -1,77 +1,99 @@
 #include <iostream>
+#include <queue>
 #include <cstring>
-#include <algorithm>
 using namespace std;
 
-
-int R, C, sX, sY;
+int R, C, sX, sY, day = 0;
+char map[1500][1500];
 int dir[4] = { -1, 0, 1, 0 };
-char map[1501][1501];
+queue<pair<int, int>> iceQ;
+queue<pair<int, int>> swanQ;
+bool visited[1500][1500] = { false, };
+bool swanVisited[1500][1500] = { false, };
 
+void putIceQueue() {
+	for (int i = 0; i<R; i++) {
+		for (int j = 0; j<C; j++) {
+			if (map[i][j] == '.' || map[i][j] == 'L') {
+				for (int d = 0; d<4; d++) {
+					int di = i + dir[d], dj = j + dir[3 - d];
+					if (di < 0 || dj < 0 || di >= R || dj >= C) continue;
+					if (map[di][dj] != 'X' || visited[di][dj]) continue;
+					visited[di][dj] = true;
+					iceQ.push({ di, dj });
+				}
+			}
+		}
+	}
 
-bool isMeet(int x, int y, bool visited[1501][1501]) {
-	for (int d = 0; d<4; d++) {
-		int dx = x + dir[d], dy = y + dir[3 - d];
-		if (dx < 0 || dy < 0 || dx > R - 1 || dy > C - 1) continue;
-		if (map[dx][dy] == 'X' || visited[dx][dy]) continue;
-		visited[dx][dy] = true;
-		if (map[dx][dy] == 'L') return true;
-		if (isMeet(dx, dy, visited)) return true;
+}
+
+void melt() {
+	if (iceQ.empty()) putIceQueue();
+	int qSize = iceQ.size();
+	for (int i = 0; i<qSize; i++) {
+		int x = iceQ.front().first, y = iceQ.front().second;
+		iceQ.pop();
+
+		map[x][y] = '.';
+
+		bool isConnect = false;
+		for (int d = 0; d<4; d++) {
+			int dx = x + dir[d], dy = y + dir[3 - d];
+			if (dx < 0 || dy < 0 || dx >= R || dy >= C) continue;
+			if (map[dx][dy] == 'X' && !visited[dx][dy]) {
+				visited[dx][dy] = true;
+				iceQ.push({ dx, dy });
+			}
+			if (!isConnect && swanVisited[dx][dy]) {
+				isConnect = true;
+				swanQ.push({ x, y });
+			}
+		}
+
+	}
+
+}
+
+bool isMeet() {
+	while (!swanQ.empty()) {
+		int x = swanQ.front().first, y = swanQ.front().second;
+		swanQ.pop();
+		swanVisited[x][y] = true;
+
+		for (int d = 0; d<4; d++) {
+			int dx = x + dir[d], dy = y + dir[3 - d];
+			if (dx < 0 || dy < 0 || dx >= R || dy >= C) continue;
+			if (map[dx][dy] == 'X' || swanVisited[dx][dy]) continue;
+			if (map[dx][dy] == 'L') return true;
+			swanVisited[dx][dy] = true;
+			swanQ.push({ dx, dy });
+		}
 	}
 
 	return false;
 }
 
-void melt() {
-	bool visited[1501][1501] = { false, };
-	for (int i = 0; i<R; i++) {
-		for (int j = 0; j<C; j++) {
-			if (map[i][j] == 'X') {
-				bool isMelt = false;
-				for (int d = 0; d<4; d++) {
-					int dx = i + dir[d], dy = j + dir[3 - d];
-					if (dx < 0 || dy < 0 || dx > R - 1 || dy > C - 1) continue;
-					if (map[dx][dy] != 'X' && !visited[dx][dy]) {
-						isMelt = true;
-						break;
-					}
-				}
-				if (isMelt) {
-					map[i][j] = '.';
-					visited[i][j] = true;
-				}
-			}
-		}
-	}
-}
-
 int main() {
 	cin >> R >> C;
-	bool isFind = false;
 	for (int i = 0; i<R; i++) {
 		cin >> map[i];
-		if (!isFind) {
-			for (int j = 0; j<C; j++) {
-				if (map[i][j] == 'L') {
-					sX = i;
-					sY = j;
-					isFind = true;
-				}
+		for (int j = 0; j<C; j++) {
+			if (map[i][j] == 'L') {
+				sX = i; sY = j;
+				break;
 			}
 		}
 	}
 
-	int date = 0;
-	bool visited[1501][1501] = { false, };
-	visited[sX][sY] = true;
-	while (!isMeet(sX, sY, visited)) {
-		memset(visited, false, sizeof(visited));
-		visited[sX][sY] = true;
-		date++;
-		melt();
-	}
+	swanQ.push({ sX, sY });
 
-	cout << date;
+	while (!isMeet()) {
+		melt();
+		day++;
+	};
+
+	cout << day;
 
 	return 0;
 }
