792a2fd143b160007bdc98c88d6956e588134b02
dks8064 <dks8064@naver.com>
2022-02-26 18:53:31 +0900

    22/02/26

--- Dynamic Programming/LCS2.cpp
+++ Dynamic Programming/LCS2.cpp
@@ -1,28 +1,49 @@
 #include <iostream>
 #include <algorithm>
+#include <cstring>
+#include <stack>
+
 using namespace std;
 
-string s1, s2, answer = "";
-string dp[1002][1002];
+string a, b;
+int dp[1002][1002];
 
 int main() {
-	cin >> s1 >> s2;
+	cin >> a >> b;
+	int aLength = a.length(), bLength = b.length();
 
-	for (int i = 0; i<s1.length(); i++) {
-		for (int j = 0; j<s2.length(); j++) {
-			if (s1[i] == s2[j]) {
-				dp[i + 1][j + 1] = dp[i][j] + s1[i];
+	for (int i = 1; i <= aLength; i++) {
+		for (int j = 1; j <= bLength; j++) {
+			if (a[i - 1] == b[j - 1]) {
+				dp[i][j] = dp[i - 1][j - 1] + 1;
 			}
 			else {
-				if (dp[i][j + 1].length() >= dp[i + 1][j].length())
-					dp[i + 1][j + 1] = dp[i][j + 1];
-				else
-					dp[i + 1][j + 1] = dp[i + 1][j];
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
 			}
 		}
 	}
 
-	cout << dp[s1.length()][s2.length()].length() << endl << dp[s1.length()][s2.length()];
+	cout << dp[aLength][bLength] << endl;
+
+	stack<char> st;
+	int i = aLength, j = bLength;
+	while (i > 0 && j > 0) {
+		if (dp[i][j] == dp[i][j - 1]) {
+			j--;
+		}
+		else if (dp[i][j] == dp[i - 1][j]) {
+			i--;
+		}
+		else {
+			i--; j--;
+			st.push(a[i]);
+		}
+	}
+
+	while (!st.empty()) {
+		cout << st.top();
+		st.pop();
+	}
 
 	return 0;
 }
